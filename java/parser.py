1ec24dcb18401887eb4f1551645bfd37e04c2af2
Raptor <raptor4694@gmail.com>
2019-07-12 12:09:41 -0500

    Updated JavaParser.parse_type_declarations()
    Added a parameter to contain the list of imports previously declared.

--- java/parser.py
+++ java/parser.py
@@ -152,7 +152,7 @@ class JavaParser:
             return self.parse_module_declaration(imports, annotations, doc)
 
         if self.token.type != ENDMARKER or modifiers or annotations:
-            types = self.parse_type_declarations(doc, modifiers, annotations)
+            types = self.parse_type_declarations(doc, modifiers, annotations, imports)
         else:
             types = []
 
@@ -276,7 +276,7 @@ class JavaParser:
         self.require(';')
         return tree.ProvidesDirective(name=name, provides=provides, doc=doc)
 
-    def parse_type_declarations(self, doc=None, modifiers=None, annotations=None) -> List[tree.TypeDeclaration]:
+    def parse_type_declarations(self, doc=None, modifiers=None, annotations=None, imports: List[tree.Import]=None) -> List[tree.TypeDeclaration]:
         types = [self.parse_type_declaration(doc, modifiers, annotations)]
         while self.token.type != ENDMARKER:
             if not self.accept(';'):
